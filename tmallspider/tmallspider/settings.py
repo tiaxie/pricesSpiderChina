6eac23959bdff2d279aeafb326d04547bb72570e
Tia Xie <xiet19@wra.net>
2020-07-14 18:00:32 +0800

    Selenium in progress

--- tmallspider/tmallspider/settings.py
+++ tmallspider/tmallspider/settings.py
@@ -18,6 +18,7 @@ NEWSPIDER_MODULE = 'tmallspider.spiders'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
+PROXY_POOL_ENABLED = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -53,6 +54,12 @@ DOWNLOADER_MIDDLEWARES = {
     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
 }
 
+#DOWNLOADER_MIDDLEWARES = {
+    # ...
+#    'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+#    'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+    # ...
+#}
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
--- tmallspider/tmallspider/spiders/tmall_spider.py
+++ tmallspider/tmallspider/spiders/tmall_spider.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 import scrapy
 from tmallspider.items import TmallspiderItem
 from tmallspider.items import JdspiderItem
@@ -9,6 +12,7 @@ from tmallspider.items import DdspiderItem
 from scrapy.utils.response import open_in_browser
 from scrapy.crawler import CrawlerRunner, CrawlerProcess
 import urllib.parse
+from selenium.webdriver.support.ui import WebDriverWait
 
 def encodeGB2312(data):
     hexData = data.encode(encoding='GB2312').hex().upper()
@@ -42,6 +46,7 @@ class tmallSpider(scrapy.Spider):
 
     def start_scraping(self, response):
         self.driver.get(response.url)
+
         self.driver.find_element_by_id('fm-login-id').send_keys('iamgooglepenn')
         self.driver.find_element_by_id('fm-login-password').send_keys('HelloWorld1_')
         self.driver.find_element_by_class_name('fm-button fm-submit password-login').click()
